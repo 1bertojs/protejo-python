316e6e70da8cec5861deaee474985c74a4152d3c
humberto <santos.humberto@aluno.ifsp.edu.br>
2025-11-20 00:12:18 -0200

    faendo o escudo

--- projeto.py
+++ projeto.py
@@ -52,21 +52,49 @@ while jogo:
     pygame.draw.rect(tela, (255, 255, 255), (580, 380, 150, 150))
     pygame.draw.rect(tela, (0, 0, 0), (587, 387, 136, 136))
     
+    #escudo
+
+    escudoc = pygame.image.load('imagens/escudo cima.png')
+    escudocima = pygame.transform.scale(escudoc, (100, 30))
+    escudob = pygame.image.load('imagens/escudo baixo.png')
+    escudobaixo = pygame.transform.scale(escudob, (100, 30))
+    escudod = pygame.image.load('imagens/escudo dir.png')
+    escudodireita = pygame.transform.scale(escudod, (30, 100))
+    escudoe = pygame.image.load('imagens/escudo esq.png')
+    escudoesquerda = pygame.transform.scale(escudoe, (30, 100))
+    esc_img = escudocima
+    esc_x = 600
+    esc_y = 405
+
+    #controles do escudo
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w or pygame.K_UP:
+                esc_img = escudocima
+                esc_x = 600
+                esc_y = 385
+            elif event.key == pygame.K_s or pygame.K_DOWN:
+                esc_img = escudobaixo
+                esc_x = 600
+                esc_y = 485
+            elif event.key == pygame.K_a or pygame.K_LEFT:
+                esc_img = escudoesquerda
+                esc_x = 550
+                esc_y = 435
+            elif event.key == pygame.K_d or pygame.K_RIGHT:
+                esc_img = escudodireita
+                esc_x = 650
+                esc_y = 435
+
     #sprites funfando
     img = pygame.image.load('imagens/coracao.png')
     coracao = pygame.transform.scale(img, (40, 40))
     tela.blit(coracao, (635, 435))
-    escudocima = pygame.image.load('imagens/escudo cima.png')
-    tela.blit(escudocima, (600, 385))
+    tela.blit(esc_img, (esc_x, esc_y))
     sprite_undyne.draw(tela)
     sprite_undyne.update()
     pygame.display.flip()
     time.tick(24)
-    
-    #controles do escudo
-    #if event.type == pygame.KEYDOWN:
-     #   if event.key == pygame.K_w or pygame.K_UP:
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
